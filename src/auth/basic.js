a811e0b4dd838d3e244089c36cd427727fd45d9f
BertoPolo <bertopolo91@gmail.com>
2022-05-24 15:43:30 +0200

    adding authors

---
+++ src/auth/basic.js
@@ -0,0 +1,21 @@
+import { createError } from "http-errors"
+import atob from "atob"
+import BlogsModel from "../services/blogs/index.js"
+
+export const basicAuthMiddleware = async (req, res, next) => {
+  if (req.headers.authorization) {
+    const base64Credentials = req.headers.authorization.split(" ")[1]
+    const { email, password } = atob(base64Credentials).split(":")
+    console.log(`EMAIL: ${email}, PASSWORD: ${password}`)
+
+    const user = await BlogsModel.checkCretials(email, password)
+    if (user) {
+      req.user = user
+      next()
+    } else {
+      next(createError(401, "Credentials are wrong!"))
+    }
+  } else {
+    next(createError(401, "provide authorization header"))
+  }
+}
